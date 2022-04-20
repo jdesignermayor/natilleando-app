479e664157aac2b970af8353cc2872ded4d30430
Jose Luis <jdesignermayor@gmail.com>
2022-04-20 14:39:15 -0500

    h1 change

--- src/pages/landing/HomeLanding.jsx
+++ src/pages/landing/HomeLanding.jsx
@@ -11,11 +11,10 @@ export const HomeLanding = () => {
       <div className='px-5 md:px-64 p-3 lg:px-96 2xl:px-[30%] grid gap-9 font-gtultraFine pt-10'>
         <div className='flex flex-col gap-5 h-screen justify-center text-center md:text-left 2xl:text-center'>
           <div className='text-7xl lg:text-8xl 2xl:text-9xl font-black'>
-            <p>Ahorra</p>
-            <p>de forma correcta</p>
+            <h1>Ahorra <br /> de forma correcta</h1>
           </div>
-          <p className='text-xl'>Natilleando es un simple fondo de ahorro para miembros exclusivos que tiene como
-          propósito crear un ahorro programado a través del tiempo con un porcentaje de rentabilidad anual.</p>
+          <h2 className='text-xl'>Natilleando es un simple fondo de ahorro para miembros exclusivos que tiene como
+          propósito crear un ahorro programado a través del tiempo con un porcentaje de rentabilidad anual.</h2>
           <TopicButtonsContainer />
         </div>
         <TopicItemContainer />
