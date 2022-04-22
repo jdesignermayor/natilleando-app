020b6b332a2e35e76b3f5a730c711ee061539527
Jose Luis <jdesignermayor@gmail.com>
2022-04-21 19:08:43 -0500

    se sube la base de datos database

---
+++ payments_database.sql
@@ -0,0 +1,94 @@
+Febrero
+
+Jhoan Velez	30000		Inscripcion,ahorro
+David Velez	100000		Inscripcion,ahorro
+Patricia	50000		Inscripcion,ahorro
+Justin Andres	100000		Inscripcion,ahorro
+Wilson	50000		Inscripcion,ahorro
+Marisol	20000		Inscripcion,ahorro
+daniela	20000		Inscripcion,ahorro
+Estefania	15000		Inscripcion,ahorro
+jose luis	300000		Inscripcion,ahorro
+Ines osorno	50000		Inscripcion,ahorro
+Marleny 	100000		Inscripcion,ahorro
+Lorenzo chimeno	20000		Inscripcion,ahorro
+Francy lopera	30000		Inscripcion,ahorro
+Sebastian 	150000	5000	Inscripcion,ahorro
+Patricia marzo	50000		ahorro
+David alejandro marzo	100000	, ahorro
+Francy lopera marzo	30000		, ahorro
+Marleny 	100000		Inscripcion, ahorro
+
+
+jhoan check:
+insert into payment_history(amount, payment_type, user_id, payment_date, tags) values ('30000', 2, 6, current_timestamp, '1' )
+insert into payment_history(amount, payment_type, user_id, payment_date, tags) values ('50000', 2, 6, current_timestamp, '1' )
+insert into payment_history(amount, payment_type, user_id, payment_date, tags) values ('50000', 2, 6, current_timestamp, '1' )
+
+Lorenzo check:
+insert into payment_history(amount, payment_type, user_id, payment_date, tags) values ('20000', 2, 7, current_timestamp, '1' );
+insert into payment_history(amount, payment_type, user_id, payment_date, tags) values ('30000', 2, 7, current_timestamp, '1' );
+insert into payment_history(amount, payment_type, user_id, payment_date, tags) values ('50000', 2, 7, current_timestamp, '1' );
+
+Justin Andres check:
+insert into payment_history(amount, payment_type, user_id, payment_date, tags, month_number) values ('100000', 2, 2, current_timestamp, '1' ,1 );
+insert into payment_history(amount, payment_type, user_id, payment_date, tags, month_number) values ('100000', 2, 2, current_timestamp, '1' ,2 );
+insert into payment_history(amount, payment_type, user_id, payment_date, tags, month_number) values ('100000', 2, 2, current_timestamp, '1' ,3 );
+
+Estefania check:
+insert into payment_history(amount, payment_type, user_id, payment_date, tags, month_number) values ('15000', 2, 5, current_timestamp, '1' ,1 );
+insert into payment_history(amount, payment_type, user_id, payment_date, tags, month_number) values ('23000', 2, 5, current_timestamp, '1' ,2 );
+insert into payment_history(amount, payment_type, user_id, payment_date, tags, month_number) values ('20000', 2, 5, current_timestamp, '1' ,3 );
+
+Wilson check:
+insert into payment_history(amount, payment_type, user_id, payment_date, tags, month_number) values ('50000', 2, 42, current_timestamp, '1' ,1 );
+insert into payment_history(amount, payment_type, user_id, payment_date, tags, month_number) values ('50000', 2, 42, current_timestamp, '1' ,2 );
+insert into payment_history(amount, payment_type, user_id, payment_date, tags, month_number) values ('50000', 2, 42, current_timestamp, '1' ,3 );
+
+Marisol check:
+insert into payment_history(amount, payment_type, user_id, payment_date, tags, month_number) values ('20000', 2, 41, current_timestamp, '1' ,1 );
+insert into payment_history(amount, payment_type, user_id, payment_date, tags, month_number) values ('20000', 2, 41, current_timestamp, '1' ,2 );
+insert into payment_history(amount, payment_type, user_id, payment_date, tags, month_number) values ('20000', 2, 41, current_timestamp, '1' ,3 );
+
+Daniela check:
+insert into payment_history(amount, payment_type, user_id, payment_date, tags, month_number) values ('20000', 2, 43, current_timestamp, '1' ,1 );
+insert into payment_history(amount, payment_type, user_id, payment_date, tags, month_number) values ('20000', 2, 43, current_timestamp, '1' ,2 );
+insert into payment_history(amount, payment_type, user_id, payment_date, tags, month_number) values ('20000', 2, 43, current_timestamp, '1' ,3 );
+
+Ines check:
+insert into payment_history(amount, payment_type, user_id, payment_date, tags, month_number) values ('50000', 2, 3, current_timestamp, '1' ,1 );
+insert into payment_history(amount, payment_type, user_id, payment_date, tags, month_number) values ('50000', 2, 3, current_timestamp, '1' ,2 );
+insert into payment_history(amount, payment_type, user_id, payment_date, tags, month_number) values ('50000', 2, 3, current_timestamp, '1' ,3 );
+
+David check:
+insert into payment_history(amount, payment_type, user_id, payment_date, tags, month_number) values ('100000', 2, 39, current_timestamp, '1' ,1 );
+insert into payment_history(amount, payment_type, user_id, payment_date, tags, month_number) values ('100000', 2, 39, current_timestamp, '1' ,2 );
+insert into payment_history(amount, payment_type, user_id, payment_date, tags, month_number) values ('100000', 2, 39, current_timestamp, '1' ,3 );
+
+Sebastian check:
+insert into payment_history(amount, payment_type, user_id, payment_date, tags, month_number) values ('50000', 2, 73, current_timestamp, '1' ,1 );
+insert into payment_history(amount, payment_type, user_id, payment_date, tags, month_number) values ('50000', 2, 73, current_timestamp, '1' ,2 );
+insert into payment_history(amount, payment_type, user_id, payment_date, tags, month_number) values ('50000', 2, 73, current_timestamp, '1' ,3 );
+
+Francy lopera check:
+insert into payment_history(amount, payment_type, user_id, payment_date, tags, month_number) values ('30000', 2, 46, current_timestamp, '1' ,1 );
+insert into payment_history(amount, payment_type, user_id, payment_date, tags, month_number) values ('30000', 2, 46, current_timestamp, '1' ,2 );
+insert into payment_history(amount, payment_type, user_id, payment_date, tags, month_number) values ('30000', 2, 46, current_timestamp, '1' ,3 );
+
+Patricia check:
+insert into payment_history(amount, payment_type, user_id, payment_date, tags, month_number) values ('50000', 2, 4, current_timestamp, '1' ,1 );
+insert into payment_history(amount, payment_type, user_id, payment_date, tags, month_number) values ('50000', 2, 4, current_timestamp, '1' ,2 );
+insert into payment_history(amount, payment_type, user_id, payment_date, tags, month_number) values ('50000', 2, 4, current_timestamp, '1' ,3 );
+insert into payment_history(amount, payment_type, user_id, payment_date, tags, month_number) values ('50000', 2, 4, current_timestamp, '1' ,4 );
+
+Jonathan check:
+insert into payment_history(amount, payment_type, user_id, payment_date, tags, month_number) values ('15000', 2, 44, current_timestamp,'1',1 );
+insert into payment_history(amount, payment_type, user_id, payment_date, tags, month_number) values ('15000', 2, 44, current_timestamp,'1',2 );
+insert into payment_history(amount, payment_type, user_id, payment_date, tags, month_number) values ('20000', 2, 44, current_timestamp,'1',3 );
+
+Marleny unchecked:
+insert into payment_history(amount, payment_type, user_id, payment_date, tags, month_number) values ('100000', 2, 45, current_timestamp,'1',1 );
+insert into payment_history(amount, payment_type, user_id, payment_date, tags, month_number) values ('50000', 2, 45, current_timestamp, '1',2 );
+insert into payment_history(amount, payment_type, user_id, payment_date, tags, month_number) values ('50000', 2, 45, current_timestamp, '1',3 );
+insert into payment_history(amount, payment_type, user_id, payment_date, tags, month_number) values ('50000', 2, 45, current_timestamp, '1',4 );
+insert into payment_history(amount, payment_type, user_id, payment_date, tags, month_number) values ('50000', 2, 45, current_timestamp, '1',5 );
