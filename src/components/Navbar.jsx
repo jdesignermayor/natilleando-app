adc98618a67258da2349438febdf23f1592a0057
Jose Luis <jdesignermayor@gmail.com>
2022-04-22 10:28:10 -0500

    se agregan nuevas validaciones para la autenticacion

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import { useAuth } from "../context/AuthContext";
+
 import Logo from '../assets/images/four-leaf.png';
 import IconSignOut from '../assets/icons/exit.svg';
-
-import { useAuth } from '../context/AuthContext';
-// import Roket from '../assets/icons/rocketWhite.svg';
 
 export const Navbar = () => {
   const { state } = useAuth();
@@ -15,9 +14,9 @@ export const Navbar = () => {
       <div className="flex items-center justify-between px-5 lg:px-40 2xl:px-[30%] h-16 p-3 gap-9 fixed w-full bg-white backdrop-filter backdrop-blur-md bg-opacity-70 border-b border-black">
         <Link
           to="/"
-          className="flex items-center font-gtultra font-bold text-xl md:text-xl"
+          className="flex items-center font-gtultra font-bold text-base"
         >
-          <img src={Logo} width="50" alt="No te quedes pelado" />
+          <img src={Logo} width="45" alt="No te quedes pelado" />
           Natilleando
         </Link>
         <div className="items-center">
