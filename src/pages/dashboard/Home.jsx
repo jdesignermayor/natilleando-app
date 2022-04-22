cbb5d1d06b36d2868b0f01f0a2b0633766c36193
Jose Luis <jdesignermayor@gmail.com>
2022-04-22 10:29:12 -0500

    se agregan los cambios de supabase

--- src/pages/dashboard/Home.jsx
+++ src/pages/dashboard/Home.jsx
@@ -1,23 +1,35 @@
-import React from 'react'
+import {useEffect } from 'react';
+
+import { useSupabase } from '../../hooks/useSupabase';
 import { Link } from 'react-router-dom';
 import { Button } from '../../components/Button';
 
 export const Home = () => {
+  const { getPaymentsById, payments, isLoading } = useSupabase();
+
+  const getPayments = () => {
+     getPaymentsById(42);
+  };
+
+  useEffect(() => {
+    console.table("payments:", payments);
+  }, [payments]);
+
   return (
     <div className="px-5 lg:px-40 2xl:px-[30%] grid gap-5 font-gtultraFine pt-20">
       <h1 className="font-black text-2xl">Resumen</h1>
       <ul className="flex flex-col gap-4">
         <li>
           <div>
-            <p className="font-bold text-3xl">Mi dinero total ahorrado 💰</p>
+            <p className="font-bold text-3xl">Total ahorrado 💰</p>
             <p className="text-2xl">$1.800.000</p>
-            <Button icon="PAYMENT" primary={false}>
+            <Button icon="PAYMENT" isLoading={isLoading} primary={false} onHandleClick={getPayments}>
               Ver todos mis aportes
             </Button>
           </div>
         </li>
         <li>
-          <div className="text-xl">
+          <div className="text-base">
             <p className="font-bold text-2xl">Total natilleando</p>
             <div className="grid gap-3 grid-cols-1 divide-y pt-4">
               <div>
