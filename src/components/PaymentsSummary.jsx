cbeb6ee3f17d9bddc32dd1b23243b415275cb19b
jose luis <jdesignermayor@gmail.com>
2022-04-24 14:37:52 -0500

    cambio tamaño de loading

--- src/components/PaymentsSummary.jsx
+++ src/components/PaymentsSummary.jsx
@@ -6,12 +6,11 @@ export const PaymentsSummary = () => {
     <ContentLoader
       speed={2}
       className="w-full"
-      height={165}
       viewBox="0 0 476 124"
       backgroundColor="#f3f3f3"
       foregroundColor="#ecebeb"
     >
-      <rect x="-2" y="5" rx="3" ry="3" width="410" height="39" />
+      <rect x="-2" y="5" rx="3" ry="3" width="410" height="60" />
     </ContentLoader>
   );
 };
