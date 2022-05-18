c5e90ab059834f942148b2536c13903cf007acd9
Jose Luis <jdesignermayor@gmail.com>
2022-05-18 12:07:35 -0500

    add responsive table styles

--- src/components/TransactionsHistoryTable.jsx
+++ src/components/TransactionsHistoryTable.jsx
@@ -7,87 +7,91 @@ export const TransactionsHistoryTable = ({ title, labelList, itemsList }) => {
     return (
       <>
         {title && <h3 className="text-xl">{title}</h3>}
-        <table className="table-auto w-full pt-4 pb-4 font-gtultra border">
-          <thead className="text-xs text-gray-700 uppercase bg-gray-50 ">
-            <tr className=" border-b">
-              {labelList?.map((name, index) => {
-                return (
-                  <th key={index} scope="col" className="px-6 py-3">
-                    {name}
-                  </th>
-                );
-              })}
-            </tr>
-          </thead>
-          <tbody>
-            {itemsList?.map(
-              ({
-                id,
-                amount,
-                payment_date,
-                username,
-                voucher_photo,
-                payment_status,
-              }) => {
-                return (
-                  <tr key={id} className="bg-white border-b rounded">
-                    <th scope="row" className="px-2 py-2  text-gray-900">
-                      <p className="font-bold">{moneyFormat(amount)}</p>
+        <div class="overflow-auto">
+          <table className="w-full h-5 overflow-y-auto pt-4 pb-4 font-gtultra border">
+            <thead className="text-xs text-gray-700 uppercase bg-gray-50 ">
+              <tr className=" border-b">
+                {labelList?.map((name, index) => {
+                  return (
+                    <th key={index} scope="col" className="px-6 py-3">
+                      {name}
                     </th>
-                    <th
-                      scope="row"
-                      className="px-2 py-2 font-medium text-gray-900 "
-                    >
-                      {payment_status === "verified" ? (
-                        <Badge color="success" name="Verificado" />
-                      ) : (
-                        <Badge color="danger" name="Pendiente" />
-                      )}
-                    </th>
-                    {username && (
-                      <th
-                        scope="row"
-                        className="px-2 py-2 font-medium text-gray-900 "
-                      >
-                        {username}
+                  );
+                })}
+              </tr>
+            </thead>
+            <tbody>
+              {itemsList?.map(
+                ({
+                  id,
+                  amount,
+                  payment_date,
+                  username,
+                  voucher_photo,
+                  payment_status,
+                }) => {
+                  return (
+                    <tr key={id} className="bg-white border-b rounded">
+                      <th scope="row" className="px-2 py-2  text-gray-900">
+                        <p className="font-bold">{moneyFormat(amount)}</p>
                       </th>
-                    )}
-                    {voucher_photo && username ? (
                       <th
                         scope="row"
                         className="px-2 py-2 font-medium text-gray-900 "
                       >
-                        <a
-                          href={voucher_photo}
-                          className="text-green-800"
-                          alt="Ver adjunto"
-                          target="_blank"
+                        {payment_status === "verified" ? (
+                          <Badge color="success" name="Verificado" />
+                        ) : (
+                          <Badge color="danger" name="Pendiente" />
+                        )}
+                      </th>
+                      {username && (
+                        <th
+                          scope="row"
+                          className="px-2 py-2 font-medium text-gray-900 "
                         >
-                          Ver adjunto
-                        </a>
-                      </th>
-                    ) : (
-                      <th
-                        scope="row"
-                        className="px-2 py-2 font-medium text-gray-900 "
-                      >
-                        <p>Sin adjunto</p>
-                      </th>
-                    )}
-                    {payment_date && (
-                      <th
-                        scope="row"
-                        className="px-2 py-2 font-medium text-gray-900 "
-                      >
-                        {payment_date ? dateFormat(payment_date) : payment_date}
-                      </th>
-                    )}
-                  </tr>
-                );
-              }
-            )}
-          </tbody>
-        </table>
+                          {username}
+                        </th>
+                      )}
+                      {voucher_photo && username ? (
+                        <th
+                          scope="row"
+                          className="px-2 py-2 font-medium text-gray-900 "
+                        >
+                          <a
+                            href={voucher_photo}
+                            className="text-green-800"
+                            alt="Ver adjunto"
+                            target="_blank"
+                          >
+                            Ver adjunto
+                          </a>
+                        </th>
+                      ) : (
+                        <th
+                          scope="row"
+                          className="px-2 py-2 font-medium text-gray-900 "
+                        >
+                          <p>Sin adjunto</p>
+                        </th>
+                      )}
+                      {payment_date && (
+                        <th
+                          scope="row"
+                          className="px-2 py-2 font-medium text-gray-900 "
+                        >
+                          {payment_date
+                            ? dateFormat(payment_date)
+                            : payment_date}
+                        </th>
+                      )}
+                    </tr>
+                  );
+                }
+              )}
+            </tbody>
+          </table>
+        </div>
       </>
     );
   } else {
